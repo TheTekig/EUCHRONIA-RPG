ec2dd9c0dcb77961028cc4748ac10f43d1e48e00
Diogo Teodoro <diogo.teodoro015@gmail.com>
2025-10-16 09:21:48 -0300

    Enhance JSON append and load functions
    Fix JSON loading and appending functionality with error handling and colored output.

--- euchronia/general_logic.py
+++ euchronia/general_logic.py
@@ -1,5 +1,25 @@
 import json
+from termcolor import colored
+
+def append_json(filepath, info):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+    
+        data.append(info)
+    
+        with open(filepath, 'w', encoding= 'utf-8') as file:
+            json.dump(data)
+
+        print(colored(f"{info} foi salvo ao banco de dados")
+              
+    except Exception:
+        print(colored(f"Erro ao salvar {info} no banco de dados") 
 
 def load_json(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
-        return json.load(file)
+        return json.load(filedata, file, indent=4, ensure_ascii=False)
+
+
+
+
